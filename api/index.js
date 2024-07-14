66dbb06c856a6d3530380fc200f76be86c2442cd
JAmal Michael <eshimi090@gmail.com>
2024-07-14 11:51:22 -1200

    signup api route

--- api/index.js
+++ api/index.js
@@ -3,20 +3,23 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/userRoutes.js';
+import authRouter from './routes/authRoutes.js';
 
 //Database connection
-// try {
-//     mongoose.connect(process.env.MONGO_DB);
-//     console.log(`connected to MongoDb`);
+try {
+    mongoose.connect(process.env.MONGO_DB);
+    console.log(`connected to MongoDb`);
 
-// } catch (error) {
-//     console.log(error);
-// }
+} catch (error) {
+    console.log(error);
+}
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log(`Server is runnig on port 3000`);
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
