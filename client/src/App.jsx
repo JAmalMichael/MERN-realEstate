0a7190ac2c44d08b3cc174b8daad639fd29d2000
JAmal Michael <eshimi090@gmail.com>
2024-07-13 22:56:33 -1200

    Pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
 
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/profile' element={<Profile />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        
+       
+      </Routes>
+   </BrowserRouter>
   )
 }
 
